08cb86c1b47682bb4a29c44b3cb37ceac28d088e
zachcp <zcharlop@rockefeller.du>
2015-05-31 19:25:09 -0400

    taking a stab at resizing; unsuccessfully

--- inst/htmlwidgets/chemdoodle.js
+++ inst/htmlwidgets/chemdoodle.js
@@ -6,13 +6,17 @@ HTMLWidgets.widget({
 
     initialize: function(el, width, height, data) {
         //return the canvas as part of the viewer data
+        var viewercanvas = new ChemDoodle.ViewerCanvas(el.id, width, height);
+
         return {
             // TODO: add instance fields as required
+           chemdoodle: viewercanvas
         }
     },
 
     resize: function(el, width, height, instance) {
-
+       if (instance.chemdoodle)
+          instance.chemdoodle.resize();
     },
 
     renderValue: function(el, x, instance) {
@@ -34,5 +38,19 @@ HTMLWidgets.widget({
         viewercanvas1.specs.atoms_font_families_2D = ['Helvetica', 'Arial', 'sans-serif'];
         viewercanvas1.specs.atoms_displayTerminalCarbonLabels_2D = true;
         viewercanvas1.loadMolecule(mol);
+
+        //Create Canvas Element
+        if (instance.chemdoodle){
+          instance.chemdoodle.specs.bonds_width_2D = 0.6;
+        instance.chemdoodle.specs.bonds_saturationWidth_2D = 0.18;
+        instance.chemdoodle.specs.bonds_hashSpacing_2D = 2.5;
+        instance.chemdoodle.specs.atoms_font_size_2D = 10;
+        instance.chemdoodle.specs.atoms_font_families_2D = ['Helvetica', 'Arial', 'sans-serif'];
+        instance.chemdoodle.specs.atoms_displayTerminalCarbonLabels_2D = true;
+        instance.chemdoodle.loadMolecule(mol);
+
+        }
+
+
     }
 });
