4f479f7b6b79417dc8c6eed1f9677a54b22c02ac
zachcp <zcharlop@rockefeller.du>
2015-05-31 20:17:30 -0400

    update viewer to allow common customization parameters passd to the JS

--- inst/htmlwidgets/chemdoodle.js
+++ inst/htmlwidgets/chemdoodle.js
@@ -6,38 +6,54 @@ HTMLWidgets.widget({
 
     initialize: function(el, width, height, data) {
         //return the canvas as part of the viewer data
-        var viewercanvas = new ChemDoodle.ViewerCanvas(el.id, width, height);
-
         return {
             // TODO: add instance fields as required
-           chemdoodle: viewercanvas
         }
     },
 
     resize: function(el, width, height, instance) {
-       if (instance.chemdoodle)
-          instance.chemdoodle.resize();
+       //if (instance.chemdoodle)
+       //   instance.chemdoodle.resize();
     },
 
     renderValue: function(el, x, instance) {
         //Check Data
-        // var y = x;
-        //console.log(y);
+        var y = x;
+        console.log(y);
 
         //Load And Scale Molecule
         var mol = new ChemDoodle.io.JSONInterpreter().molFrom( x.json );
         mol.scaleToAverageBondLength( x.bondscale );
-        //console.log(mol);
 
         //Create Canvas Element
         var viewercanvas1 = new ChemDoodle.ViewerCanvas("viewer2", x.width, x.height);
-        viewercanvas1.specs.bonds_width_2D = 0.6;
+
         viewercanvas1.specs.bonds_saturationWidth_2D = 0.18;
         viewercanvas1.specs.bonds_hashSpacing_2D = 2.5;
-        viewercanvas1.specs.atoms_font_size_2D = 10;
-        viewercanvas1.specs.atoms_font_families_2D = ['Helvetica', 'Arial', 'sans-serif'];
-        viewercanvas1.specs.atoms_displayTerminalCarbonLabels_2D = true;
+        viewercanvas1.specs.backgroundColor = x.backgroundColor;
+        viewercanvas1.specs.scale = x.scale;
+        // atom display info
+        viewercanvas1.specs.atoms_font_size_2D = x.atoms_font_size_2D;
+        viewercanvas1.specs.atoms_font_families_2D = x.atoms_font_families_2D;
+        viewercanvas1.specs.atoms_font_italic_2D = x.atoms_font_italic_2D;
+        viewercanvas1.specs.atoms_circleDiameter_2D = x.atoms_circleDiameter_2D;
+        viewercanvas1.specs.atoms_useJMOLColors = x.atoms_useJMOLColors;
+        viewercanvas1.specs.atoms_useJMOLColors = x.atoms_useJMOLColors;
+        viewercanvas1.specs.atoms_usePYMOLColors = x.atoms_usePYMOLColors;
+        viewercanvas1.specs.atoms_implicitHydrogens_2D = x.atoms_implicitHydrogens_2D;
+        viewercanvas1.specs.atoms_displayTerminalCarbonLabels_2D = x.atoms_displayTerminalCarbonLabels_2D;
+        viewercanvas1.specs.atoms_displayAllCarbonLabels_2D = x.atoms_displayAllCarbonLabels_2D;
+        // bond display info
+        viewercanvas1.specs.bonds_display = x.bonds_display;
+        viewercanvas1.specs.bonds_width_2D = x.bonds_width_2D;
+        viewercanvas1.specs.bonds_saturationWidth_2D = x.bonds_saturationWidth_2D;
+        viewercanvas1.specs.bonds_ends_2D = x.bonds_ends_2D;
+        viewercanvas1.specs.bonds_useJMOLColors = x.bonds_useJMOLColors;
+        viewercanvas1.specs.bonds_usePYMOLColors = x.bonds_usePYMOLColors;
+        viewercanvas1.specs.bonds_wedgeThickness_2D = x.bonds_wedgeThickness_2D;
+        viewercanvas1.specs.bonds_hashWidth_2D = x.bonds_hashWidth_2D;
         viewercanvas1.loadMolecule(mol);
+
 
         //Create Canvas Element
         if (instance.chemdoodle){
