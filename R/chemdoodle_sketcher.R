f0c1cfd83a0dfc1b07fc5641a25fced18c1ec22e
zach charlop-powers <zach.charlop.powers@gmail.com>
2016-03-14 12:48:04 -0400

    basic sketcher

---
+++ R/chemdoodle_sketcher.R
@@ -0,0 +1,113 @@
+#' ChemDoodle Sketcher Canvas
+#'
+#' Widget that creates a ChemDoodle Sketcher
+#'
+#' @import htmlwidgets
+#'
+#' #: background color of the canvas, set this to undefined to create a see-through canvas
+#' scale of the canvas, set this after the molecule has been loaded, then repaint the canvas
+#'  atom text font size for 2D depiction
+#'  the atom text font families, families cascade through the array if not found on the users computer; for 2D depiction
+#'   atom text will be italicized
+#'   diameter of atom circles for 2D depiction
+#'    use Jmol colors for atoms
+#'    use PyMOL colors for atoms, will default to Jmol if that specification is also true
+#'    render implicit hydrogens on all labels that are visible
+#'    show labels for terminal carbons
+#'    show all carbon labels
+#'     draw the bonds
+#'     width of the bonds for 2D depiction; also controls the width of primitive lines for bonds rendered in WebGL scenes
+#'     relative saturation width of double and triple bond lines for 2D depiction
+#'     bond end style for 2D depiction Allowed Values: [butt, round, square],
+#'     color the bond by using the Jmol colors of the connected atoms, the type of fill is controlled by the bonds_colorGradient specification
+#'     color the bond by using the PyMOL colors of the connected atoms, the type of fill is controlled by the bonds_colorGradient specification, will default to Jmol if that specification is also true
+#'     color the bond by using a gradient between the two colors of the constituent atoms, rather than by using a color split
+#'     double bonds are drawn symmetrically always, instead of pointing towards the center of a ring, for instance; for 2D depiction
+#'
+#'
+#' @export
+chemdoodle_sketcher <- function(
+                              width = 500,
+                              height = 500,
+                              bondscale=14.4,
+                              #canvas settings
+                              backgroundColor="white",
+                              scale= 1,
+                              #atom settings
+                              atoms_font_size_2D = 12,
+                              atoms_font_families_2D = c("Helvetica", "Arial", "Dialog"),
+                              atoms_font_italic_2D= FALSE,
+                              atoms_circleDiameter_2D = 10,
+                              atoms_useJMOLColors = FALSE,
+                              atoms_usePYMOLColors = FALSE,
+                              atoms_implicitHydrogens_2D = TRUE,
+                              atoms_displayTerminalCarbonLabels_2D = TRUE,
+                              atoms_displayAllCarbonLabels_2D = FALSE,
+                              # bond specifications
+                              bonds_display = TRUE,
+                              bonds_width_2D = 1,
+                              bonds_saturationWidth_2D = 0.2,
+                              bonds_ends_2D = "round",
+                              bonds_useJMOLColors = FALSE,
+                              bonds_usePYMOLColors = FALSE,
+                              bonds_wedgeThickness_2D = 0.22,
+                              bonds_hashWidth_2D = 1) {
+
+  # forward options using x
+  x = list(
+    width = width,
+    height = height,
+    bondscale = bondscale,
+    backgroundColor="white",
+    scale = scale,
+    atoms_font_size_2D = atoms_font_size_2D,
+    atoms_font_families_2D =atoms_font_families_2D,
+    atoms_font_italic_2D = atoms_font_italic_2D,
+    atoms_circleDiameter_2D = atoms_circleDiameter_2D,
+    atoms_useJMOLColors = atoms_useJMOLColors,
+    atoms_useJMOLColors = atoms_useJMOLColors,
+    atoms_usePYMOLColors = atoms_usePYMOLColors,
+    atoms_implicitHydrogens_2D = atoms_implicitHydrogens_2D,
+    atoms_displayTerminalCarbonLabels_2D = atoms_displayTerminalCarbonLabels_2D,
+    atoms_displayAllCarbonLabels_2D = atoms_displayAllCarbonLabels_2D,
+    bonds_display = bonds_display,
+    bonds_width_2D = bonds_width_2D,
+    bonds_saturationWidth_2D = bonds_saturationWidth_2D,
+    bonds_ends_2D = bonds_ends_2D,
+    bonds_useJMOLColors = bonds_useJMOLColors,
+    bonds_usePYMOLColors = bonds_usePYMOLColors,
+    bonds_wedgeThickness_2D = bonds_wedgeThickness_2D,
+    bonds_hashWidth_2D = bonds_hashWidth_2D
+  )
+
+  #modify JSON serialization
+  attr(x, 'TOJSON_ARGS') <- list(dataframe=c("rows"))
+
+  # create widget
+  htmlwidgets::createWidget(
+    name = 'chemdoodle_sketcher',
+    x = x,
+    width = width,
+    height = height,
+    package = 'chemdoodle',
+    sizingPolicy = htmlwidgets::sizingPolicy(
+      viewer.padding = 0,
+      viewer.paneHeight = 500,
+      browser.fill = TRUE)
+  )
+}
+
+#' Widget output function for use in Shiny
+#'
+#' @export
+chemdoodle_sketcherOutput <- function(outputId, width = '100%', height = '400px'){
+  shinyWidgetOutput(outputId, 'chemdoodle', width, height, package = 'chemdoodle')
+}
+
+#' Widget render function for use in Shiny
+#'
+#' @export
+renderChemdoodle <- function(expr, env = parent.frame(), quoted = FALSE) {
+  if (!quoted) { expr <- substitute(expr) } # force quoted
+  shinyRenderWidget(expr, chemdoodleOutput, env, quoted = TRUE)
+}
---
+++ inst/htmlwidgets/chemdoodle_sketcher.js
@@ -0,0 +1,65 @@
+HTMLWidgets.widget({
+
+  name: 'chemdoodle_sketcher',
+
+  type: 'output',
+
+  initialize: function(el, width, height, data) {
+    //return the canvas as part of the viewer data
+    return {
+      // TODO: add instance fields as required
+    }
+  },
+
+  resize: function(el, width, height, instance) {
+    //if (instance.chemdoodle)
+      //   instance.chemdoodle.resize();
+  },
+
+  renderValue: function(el, x, instance) {
+    var id = el.id;
+    // remove any previously created canvas element
+    el.innerHTML = "";
+
+    // make a canvas with the correct Id
+    var canvasId = id + "-canvas";
+    var canvas = document.createElement("canvas");
+    canvas.id = canvasId;
+    canvas.width = el.offsetWidth;
+    canvas.height = el.offsetHeight;
+    el.appendChild(canvas);
+
+    //Create Canvas Element
+
+    var sketchercanvas = new ChemDoodle.SketcherCanvas(canvas.id,
+                                                       x.width,
+                                                       x.height,
+                                                       {useServices:false, oneMolecule:true});
+
+    sketchercanvas.specs.bonds_saturationWidth_2D = 0.18;
+    sketchercanvas.specs.bonds_hashSpacing_2D = 2.5;
+    sketchercanvas.specs.backgroundColor = x.backgroundColor;
+    sketchercanvas.specs.scale = x.scale;
+    // atom display info
+    sketchercanvas.specs.atoms_font_size_2D = x.atoms_font_size_2D;
+    sketchercanvas.specs.atoms_font_families_2D = x.atoms_font_families_2D;
+    sketchercanvas.specs.atoms_font_italic_2D = x.atoms_font_italic_2D;
+    sketchercanvas.specs.atoms_circleDiameter_2D = x.atoms_circleDiameter_2D;
+    sketchercanvas.specs.atoms_useJMOLColors = x.atoms_useJMOLColors;
+    sketchercanvas.specs.atoms_useJMOLColors = x.atoms_useJMOLColors;
+    sketchercanvas.specs.atoms_usePYMOLColors = x.atoms_usePYMOLColors;
+    sketchercanvas.specs.atoms_implicitHydrogens_2D = x.atoms_implicitHydrogens_2D;
+    sketchercanvas.specs.atoms_displayTerminalCarbonLabels_2D = x.atoms_displayTerminalCarbonLabels_2D;
+    sketchercanvas.specs.atoms_displayAllCarbonLabels_2D = x.atoms_displayAllCarbonLabels_2D;
+    // bond display info
+    sketchercanvas.specs.bonds_display = x.bonds_display;
+    sketchercanvas.specs.bonds_width_2D = x.bonds_width_2D;
+    sketchercanvas.specs.bonds_saturationWidth_2D = x.bonds_saturationWidth_2D;
+    sketchercanvas.specs.bonds_ends_2D = x.bonds_ends_2D;
+    sketchercanvas.specs.bonds_useJMOLColors = x.bonds_useJMOLColors;
+    sketchercanvas.specs.bonds_usePYMOLColors = x.bonds_usePYMOLColors;
+    sketchercanvas.specs.bonds_wedgeThickness_2D = x.bonds_wedgeThickness_2D;
+    sketchercanvas.specs.bonds_hashWidth_2D = x.bonds_hashWidth_2D;
+
+  }
+});
