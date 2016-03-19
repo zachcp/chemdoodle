c57a5eb1d90afe27c228031c98dfb001fd17f108
zach charlop-powers <zach.charlop.powers@gmail.com>
2016-03-19 19:13:51 -0400

    create a toChemDoodle funciton which converts AtomContainers to ChemDoodle structure

--- R/chemdoodle_json_utilities.R
+++ R/chemdoodle_json_utilities.R
@@ -132,3 +132,54 @@ toSmiles <- function(mol){
   smigen$create(mol)
   return(smigen$create(mol))
 }
+
+#' CDK AtomContainer to ChemDoodle JSON
+#'
+#' @export
+toChemDoodle <- function(mol){
+  
+  # check for 2D coordinates
+  coords <- lapply( 0:(mol$getAtomCount()-1),
+    function(x) {
+      atom <- mol$getAtom(x)
+      return(atom$getPoint2d())
+  })
+  
+  needscoords <- any( sapply(coords, is.null) )
+  
+  if (needscoords ==  TRUE) {
+    mol <- generate.2d.coordinates(mol)
+  }
+  
+  jmol <- list(a=list(),b=list())
+  
+  #loop through atoms
+  for (i in 0:(mol$getAtomCount()-1)) {
+    atom       <- mol$getAtom(i)
+    atomtype   <- atom$getSymbol()
+    atomcoords <- atom$getPoint2d()
+    atomdata <- list(l = atomtype, x = atomcoords$x, y = atomcoords$y)
+    #print(atomdata)
+    jmol$a[i] <- list(atomdata)
+  }
+  
+  #loop through bonds
+  for (i in 0:(mol$getBondCount()-1)) {
+    bond       <- mol$getBond(i)
+    atom1      <- bond$getAtom(0L)
+    atom2      <- bond$getAtom(1L)
+    atom1n     <- mol$getAtomNumber(atom1)
+    atom2n     <- mol$getAtomNumber(atom2)
+    bondorder  <- bond$getOrder()$toString()
+    
+    if (bondorder == "SINGLE") bondnumber <- 1  
+    if (bondorder == "DOUBLE") bondnumber <- 2
+    if (bondorder == "TRIPLE") bondnumber <- 3
+    
+    
+    list(b = atom1n, e = atom2n , o = bondorder)
+    jmol$b[i] <- list(bonddata)
+  }
+  
+  return(jmol)
+}
